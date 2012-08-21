5970eb744644d577458042a5273916cf8d649aaf
Team3 <fieldrobot2012team3@madsbahrt.dk>
2012-08-21 14:56:01 +0200

    send function and callback

--- unitylink/src/unitylink.cpp
+++ unitylink/src/unitylink.cpp
@@ -3,27 +3,48 @@
 
 #include <sstream>
 #include "fmMsgs/serial.h"
+#include <boost/thread.hpp>
 
-int main(int argc, char **argv){
-  ros::init(argc, argv, "talker");
-  ros::NodeHandle n;
-  ros::Publisher tx_pub = n.advertise<fmMsgs::serial>("S0_tx_msg", 20);
-  ros::Rate loop_rate(10);
-  fmMsgs::serial serial_tx_msg;
-  serial_tx_msg.header.seq = 0;
+ros::Publisher tx_pub;
+fmMsgs::serial serial_tx_msg;
 
-  while (ros::ok())
-  {
+
+void serialCallback(const fmMsgs::serial::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  //gem data
+  //unlock
+}
+
+int sendMsg(std::string s){
     ++serial_tx_msg.header.seq;
-    serial_tx_msg.data = "#R:00\n";
+    std::stringstream ss;
+    ss << s << "\n";
+    serial_tx_msg.data = ss.str();
     serial_tx_msg.header.stamp = ros::Time::now();
-    //std::stringstream ss;
-    //ss << "hello world " << count;
-    //msg.data = ss.str();
 
     ROS_INFO("%s", serial_tx_msg.data.c_str());
 
     tx_pub.publish(serial_tx_msg);
+    return 0;
+}
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+  tx_pub = n.advertise<fmMsgs::serial>("S0_tx_msg", 20);
+  ros::Rate loop_rate(10);
+  serial_tx_msg.header.seq = 0;
+  ros::Subscriber serialsub = n.subscribe("S0_rx_msg", 20, serialCallback);
+  
+
+  while (ros::ok())
+  {
+    sendMsg("#R:00");
+    //lock();
+
+    //skriv resultat til nyt rostopic
+    //send næste fors
 
     ros::spinOnce();
 
