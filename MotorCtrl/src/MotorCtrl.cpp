35307235aa2bb82a87069befc9319e92218210ea
Team3 <fieldrobot2012team3@madsbahrt.dk>
2012-08-22 16:57:26 +0200

    MotorCtrl done

--- MotorCtrl/src/MotorCtrl.cpp
+++ MotorCtrl/src/MotorCtrl.cpp
@@ -1,85 +1,46 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "MotorCtrl/MotorCtrlMsg.h"
+#include <boost/lexical_cast.hpp>
+#include <boost/format.hpp>
+ //   using boost::bad_lexical_cast;
+
 
 #include <sstream>
+#define NEGATIVE 0b1000000000000000
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+ros::Publisher ul_pub;
+
+std::string convert(int16_t input){
+	if(input > 10000) input = 10000;
+	if(input < -10000) input = -10000;
+	if(input < 0) {
+		input = abs(input);
+		input = input|NEGATIVE;
+	}
+	std::stringstream ss;
+	ss << boost::format(
+				"%04X")
+				% input;
+	return ss.str();
+}
+
+void motorCtrlCallback(const MotorCtrl::MotorCtrlMsg::ConstPtr& in_msg)
+{
+	ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l_speed, in_msg->r_speed);
+	std_msgs::String out_msg;
+	std::string out_string = convert(in_msg->l_speed) + convert(in_msg->r_speed);
+	ROS_INFO("I send: [%s]", out_string.c_str());
+	out_msg.data = out_string;
+	ul_pub.publish(out_msg);
+}
+
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "talker");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
-
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-
-  ros::Rate loop_rate(10);
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
-  {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
-
-
-  return 0;
+	ros::init(argc, argv, "MotorCtrl");
+	ros::NodeHandle n;
+	ul_pub = n.advertise<std_msgs::String>("ULREG_W05", 20);
+	ros::Subscriber sub = n.subscribe("MotorCtrl", 20, motorCtrlCallback);
+	ros::spin();
+	return 0;
 }
