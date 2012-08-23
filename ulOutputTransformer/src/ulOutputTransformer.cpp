3dbe1056237e86c80c310805beabd1488a1b7c81
Team3 <fieldrobot2012team3@madsbahrt.dk>
2012-08-23 09:30:13 +0200

    ulOutputTransformer main method done

--- ulOutputTransformer/src/ulOutputTransformer.cpp
+++ ulOutputTransformer/src/ulOutputTransformer.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 #include "ulOutputTransformer/encPosMsg.h"
+#include "ulOutputTransformer/encSpdMsg.h"
 #include <boost/lexical_cast.hpp>
 #include <boost/format.hpp>
  //   using boost::bad_lexical_cast;
@@ -9,7 +11,9 @@
 #include <sstream>
 #define NEGATIVE 0b1000000000000000
 
-ros::Publisher ul_pub;
+ros::Publisher enc_pos_pub;
+ros::Publisher enc_spd_pub;
+ros::Publisher touch_pub;
 
 std::string convert(int16_t input){
 	if(input > 10000) input = 10000;
@@ -25,7 +29,26 @@ std::string convert(int16_t input){
 	return ss.str();
 }
 
-void motorCtrlCallback(const ulOutputTransformer::encPosMsg::ConstPtr& in_msg)
+void ulR01Callback(const std_msgs::String::ConstPtr& in_msg)
+{
+	//ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l, in_msg->r);
+	std_msgs::String out_msg;
+	//std::string out_string = convert(in_msg->l) + convert(in_msg->r);
+	//ROS_INFO("I send: [%s]", out_string.c_str());
+	//out_msg.data = out_string;
+	//ul_pub.publish(out_msg);
+}
+
+void ulR02Callback(const std_msgs::String::ConstPtr& in_msg)
+{
+	//ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l, in_msg->r);
+	std_msgs::String out_msg;
+	//std::string out_string = convert(in_msg->l) + convert(in_msg->r);
+	//ROS_INFO("I send: [%s]", out_string.c_str());
+	//out_msg.data = out_string;
+	//ul_pub.publish(out_msg);
+}
+void ulR03Callback(const std_msgs::String::ConstPtr& in_msg)
 {
 	//ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l, in_msg->r);
 	std_msgs::String out_msg;
@@ -37,10 +60,14 @@ void motorCtrlCallback(const ulOutputTransformer::encPosMsg::ConstPtr& in_msg)
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "MotorCtrl");
+	ros::init(argc, argv, "ulOutputTransformer");
 	ros::NodeHandle n;
-	ul_pub = n.advertise<std_msgs::String>("ULREG_W05", 20);
-	ros::Subscriber sub = n.subscribe("MotorCtrl", 20, motorCtrlCallback);
+	enc_pos_pub = n.advertise<ulOutputTransformer::encPosMsg>("enc_pos", 20);
+	enc_spd_pub = n.advertise<ulOutputTransformer::encSpdMsg>("enc_spd", 20);
+	touch_pub = n.advertise<std_msgs::Bool>("start_btn", 20);
+	ros::Subscriber r01sub = n.subscribe("ULREG_R01", 20, ulR01Callback);
+	ros::Subscriber r02sub = n.subscribe("ULREG_R02", 20, ulR02Callback);
+	ros::Subscriber r03sub = n.subscribe("ULREG_R03", 20, ulR03Callback);
 	ros::spin();
 	return 0;
 }
