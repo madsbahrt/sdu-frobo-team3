c65708e30788d336e8b6f2e406a21ca92ddd6f7b
Team3 <fieldrobot2012team3@madsbahrt.dk>
2012-08-25 17:44:45 +0200

    ulOutpuTransformer done?

--- ulOutputTransformer/src/ulOutputTransformer.cpp
+++ ulOutputTransformer/src/ulOutputTransformer.cpp
@@ -5,9 +5,11 @@
 #include "ulOutputTransformer/encSpdMsg.h"
 #include <boost/lexical_cast.hpp>
 #include <boost/format.hpp>
+#include <time.h>
+
 
 #include <sstream>
-#define NEGATIVE 0b1000000000000000
+#define NEGATIVE_BIT 0b1000000000000000
 
 #define ASCII_0 48
 #define ASCII_1 49
@@ -32,137 +34,149 @@
 #define ASCII_F 70
 #define ASCII_f 102
 
+#define NEGATIVE_BIT 0b1000000000000000
+#define NUMBER_BITS  0b0111111111111111
+#define START_BIT    0x00000001
+
+
 ros::Publisher enc_pos_pub;
 ros::Publisher enc_spd_pub;
-ros::Publisher touch_pub;
+ros::Publisher start_press_pub;
+time_t lastpress;
 
-int16_t convert(std::string input){
+uint32_t hexstr2uint_32conv(std::string input){
 
-	int results[8];
-
+	uint32_t inttempresult;
+	uint32_t shiftedresult;
 	uint32_t result;
-
-	bool negative = false;
 
 	for(int i = 0; i<8; i++){
 		switch ( input.at(i) ) {
 
 		case ASCII_0 :
-			results[i] = 0;
+			inttempresult = 0;
 			break;
 		case ASCII_1 :
-			results[i] = 1;
+			inttempresult = 1;
 			break;
 		case ASCII_2 :
-			results[i] = 2;
+			inttempresult = 2;
 			break;
 		case ASCII_3 :
-			results[i] = 3;
+			inttempresult = 3;
 			break;
 		case ASCII_4 :
-			results[i] = 4;
+			inttempresult = 4;
 			break;
 		case ASCII_5 :
-			results[i] = 5;
+			inttempresult = 5;
 			break;
 		case ASCII_6 :
-			results[i] = 6;
+			inttempresult = 6;
 			break;
 		case ASCII_7 :
-			results[i] = 7;
+			inttempresult = 7;
 			break;
 		case ASCII_8 :
-			results[i] = 8;
+			inttempresult = 8;
 			break;
 		case ASCII_9 :
-			results[i] = 9;
+			inttempresult = 9;
 			break;
 		case ASCII_A :
 		case ASCII_a :
-			results[i] = 10;
+			inttempresult = 10;
 			break;
 		case ASCII_B :
 		case ASCII_b :
-			results[i] = 11;
+			inttempresult = 11;
 			break;
 		case ASCII_C :
 		case ASCII_c :
-			results[i] = 12;
+			inttempresult = 12;
 			break;
 		case ASCII_D :
 		case ASCII_d :
-			results[i] = 13;
+			inttempresult = 13;
 			break;
 		case ASCII_E :
 		case ASCII_e :
-			results[i] = 14;
+			inttempresult = 14;
 			break;
 		case ASCII_F :
 		case ASCII_f :
-			results[i] = 15;
+			inttempresult = 15;
 			break;
 		default :
 			ROS_ERROR("Tried to hexconvert character ", input.at(i));
 			break;
 		}
 
-		if(i == 0 && result[i] > 7){
-
-		}
-
-
-		ROS_INFO("Hexconverted [%s] to [%i]", input.substr(i, 1).c_str(), results[i]);
+		ROS_INFO("Hexconverted [%s] to [%i]", input.substr(i, 1).c_str(), inttempresult);
+		shiftedresult = inttempresult << ((7-i)*4);
+		result += shiftedresult;
+		ROS_INFO("shifted [%X] to [%X], accumulated result now [%X]", inttempresult, shiftedresult, result);
 	}
-	return 0;//ss.str();
+	return result;
+}
+
+int16_t uint2int(uint16_t input){
+	int16_t result = 0;
+	if((input & NEGATIVE_BIT) == 0){ //positive number
+		result = input;
+    } else { //negative number
+    	result = -(input & NUMBER_BITS);
+    }
+	ROS_INFO("Signconverted [%X] to [%i]", input, result);
+	return result;
 }
 
 void ulR01Callback(const std_msgs::String::ConstPtr& in_msg)
 {
+	ulOutputTransformer::encPosMsg encPosMsg;
 	ROS_INFO("I heard: [%s]", in_msg->data.c_str());
-    convert(in_msg->data);
+    uint32_t raw_bits = hexstr2uint_32conv(in_msg->data);
 
-//	ROS_INFO("Converted to: [%d]", x);
+    uint16_t high_bits = raw_bits >> 16;
+    uint16_t low_bits = raw_bits;
+    ROS_INFO("Highbits [%X] Lowbits [%X]", high_bits, low_bits);
 
-
-//	using boost::lexical_cast;
-//	using boost::bad_lexical_cast;
-
-//	std::stringstream ss;
-//	ss << "0x" << in_msg->data;
-//
-//	try {
-//	    uint32_t x = lexical_cast<uint32_t>(ss.str());
-//	    ROS_INFO("Converted to: [%d]", x);
-//	} catch(bad_lexical_cast &) {
-//	    ROS_ERROR("Failed to convert string %s", ss.str().c_str());
-//	}
-
-
-	std_msgs::String out_msg;
-	//std::string out_string = convert(in_msg->l) + convert(in_msg->r);
-	//ROS_INFO("I send: [%s]", out_string.c_str());
-	//out_msg.data = out_string;
-	//ul_pub.publish(out_msg);
+    encPosMsg.l_pos = uint2int(high_bits);
+    encPosMsg.r_pos = uint2int(low_bits);
+    enc_pos_pub.publish(encPosMsg);
 }
 
 void ulR02Callback(const std_msgs::String::ConstPtr& in_msg)
 {
-	//ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l, in_msg->r);
-	std_msgs::String out_msg;
-	//std::string out_string = convert(in_msg->l) + convert(in_msg->r);
-	//ROS_INFO("I send: [%s]", out_string.c_str());
-	//out_msg.data = out_string;
-	//ul_pub.publish(out_msg);
+	ulOutputTransformer::encSpdMsg encSpdMsg;
+	ROS_INFO("I heard: [%s]", in_msg->data.c_str());
+    uint32_t raw_bits = hexstr2uint_32conv(in_msg->data);
+
+    uint16_t high_bits = raw_bits >> 16;
+    uint16_t low_bits = raw_bits;
+    ROS_INFO("Highbits [%X] Lowbits [%X]", high_bits, low_bits);
+
+    encSpdMsg.l_speed = uint2int(high_bits);
+    encSpdMsg.r_speed = uint2int(low_bits);
+    enc_spd_pub.publish(encSpdMsg);
 }
 void ulR03Callback(const std_msgs::String::ConstPtr& in_msg)
 {
-	//ROS_INFO("I heard: Left[%d] Right[%d]", in_msg->l, in_msg->r);
-	std_msgs::String out_msg;
-	//std::string out_string = convert(in_msg->l) + convert(in_msg->r);
-	//ROS_INFO("I send: [%s]", out_string.c_str());
-	//out_msg.data = out_string;
-	//ul_pub.publish(out_msg);
+	ROS_INFO("I heard: [%s]", in_msg->data.c_str());
+    uint32_t raw_bits = hexstr2uint_32conv(in_msg->data);
+    if(((raw_bits  & START_BIT)!=0) && (time (NULL) - lastpress > 20)){ //start
+    	std_msgs::Bool startMsg;
+    	startMsg.data = true;
+    	lastpress = time (NULL);
+
+    	start_press_pub.publish(startMsg);
+
+    } else {
+    	ROS_INFO("Startbit false");
+    }
+    ROS_INFO("Timer: [%i]", time (NULL));
 }
+
 
 int main(int argc, char **argv)
 {
@@ -170,11 +184,11 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	enc_pos_pub = n.advertise<ulOutputTransformer::encPosMsg>("enc_pos", 20);
 	enc_spd_pub = n.advertise<ulOutputTransformer::encSpdMsg>("enc_spd", 20);
-	touch_pub = n.advertise<std_msgs::Bool>("start_btn", 20);
+	start_press_pub = n.advertise<std_msgs::Bool>("start_btn", 20);
 	ros::Subscriber r01sub = n.subscribe("ULREG_R01", 20, ulR01Callback);
 	ros::Subscriber r02sub = n.subscribe("ULREG_R02", 20, ulR02Callback);
 	ros::Subscriber r03sub = n.subscribe("ULREG_R03", 20, ulR03Callback);
-
+	lastpress = time (NULL);
 	ros::spin();
 	return 0;
 }
