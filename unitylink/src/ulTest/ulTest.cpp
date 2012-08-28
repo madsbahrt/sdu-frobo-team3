45ced7ed444c277b3477e7b10372747026390fa9
Team3 <fieldrobot2012team3@madsbahrt.dk>
2012-08-28 10:29:52 +0200

    Added MIT Licence clauses

--- unitylink/src/ulTest/ulTest.cpp
+++ unitylink/src/ulTest/ulTest.cpp
@@ -28,41 +28,27 @@ OTHER DEALINGS IN THE SOFTWARE.
  */
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "MotorCtrl/MotorCtrlMsg.h"
-#include <boost/lexical_cast.hpp>
-#include <boost/format.hpp>
-#include "geometry_msgs/TwistStamped.h"
- //   using boost::bad_lexical_cast;
-
 
 #include <sstream>
 
-ros::Publisher motorCtrl_pub;
+ros::Publisher ul_pub;
 
-void motorCtrlCallback(const geometry_msgs::TwistStamped::ConstPtr& in_msg)
+void ulCallback(const std_msgs::String::ConstPtr& in_msg)
 {
-	float linear = in_msg->twist.linear.x;
-	float angular = in_msg->twist.angular.z;
-	ROS_DEBUG("I heard: Linear[%f] Angular[%f]", linear , angular);
-	int left = 10000 * linear - 10000*angular;
-
-	int right = 10000 * linear + 10000*angular;
-
-	MotorCtrl::MotorCtrlMsg out_msg;
-
-	ROS_DEBUG("Transform: Left[%i] Right[%i]", left, right);
-
-	out_msg.l_speed = left;
-	out_msg.r_speed = right;
-	motorCtrl_pub.publish(out_msg);
+	ul_pub.publish(in_msg);
+}
+void triggerCallback(const std_msgs::String::ConstPtr& in_msg)
+{
+	ul_pub.publish(in_msg);
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "MotorCtrl");
+	ros::init(argc, argv, "ulTest");
 	ros::NodeHandle n;
-	motorCtrl_pub = n.advertise<MotorCtrl::MotorCtrlMsg>("MotorCtrl", 20);
-	ros::Subscriber sub = n.subscribe("fmHMI/wii_cmd_vel", 20, motorCtrlCallback);
+	ul_pub = n.advertise<std_msgs::String>("ULREG_W06", 20);
+	ros::Subscriber sub = n.subscribe("ULREG_R03", 20, ulCallback);
+	ros::Subscriber triggersub = n.subscribe("trigger", 20, ulCallback);
 	ros::spin();
 	return 0;
 }
